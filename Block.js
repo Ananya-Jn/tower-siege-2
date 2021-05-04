c4630891ed0d5cf136b157db1284fa3f53fa7dfd
Ananya-Jn <75167161+Ananya-Jn@users.noreply.github.com>
2021-05-04 13:14:58 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -1,25 +1,29 @@
 class Block{
-  constructor(x, y){
-    super(x,y,50,50);
-    this.image = loadImage("block.png");
-    this.Visibility = 255;
-  }
-  display(){
-       
-    if(this.body.speed < 3)
-      super.display();
-
-      else{
-        World.remove(world,this.body)
-        push();
-        tint(255, this.Visibility);
-        image(this.image,this.body.position.x,this.body.position.y);
-        this.Visibility -= 5;
-        pop();
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
       }
-    
-    console.log(this.body.speed);
-
-    
-  }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image= loadImage('block.png')
+      this.Visiblity = 255;
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed < 3){
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+    }
+ else{
+   World.remove(world, this.body);
+   push();
+   this.Visiblity = this.Visiblity - 5;
+   tint(255,this.Visiblity);
+   image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+   pop();
+ }
+}
 }
